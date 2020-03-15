d930e7635981692c584c69ec508a603aee0fb5a0
Egemen <uzunaliegemen@gmail.com>
2020-03-15 21:41:02 +0100

    updated config with a temp prodendpoint

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,5 +1,3 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:5000`;
-export const prodEndpoint = 'prodendpoint';
-export const prodUrl = 'produrl';
-export const placeholder = 'https://material-ui.com/static/images/avatar/1.jpg';
+export const prodEndpoint = 'http://localhost:5000';

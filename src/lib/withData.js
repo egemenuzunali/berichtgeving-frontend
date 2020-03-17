3f63d4625a7d6f18325c18af9a8037a41fe384cf
Egemen <uzunaliegemen@gmail.com>
2020-03-17 12:51:41 +0100

    send auth token for every request

--- src/lib/withData.js
+++ src/lib/withData.js
@@ -1,15 +1,29 @@
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
 import { endpoint, prodEndpoint } from './config';
 
 const link = createHttpLink({
 	uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
 });
 
+const authLink = setContext((_, { headers }) => {
+	// get the authentication token from local storage if it exists
+	const token = localStorage.getItem('token');
+	// return the headers to the context so httpLink can read them
+	console.log(token);
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : '',
+		},
+	};
+});
+
 function createClient() {
 	return new ApolloClient({
-		link,
+		link: authLink.concat(link),
 		cache: new InMemoryCache(),
 	});
 }
